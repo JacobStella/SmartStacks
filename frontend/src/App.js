8d48974c6eb6c81eb8077759169fc9f16a8d1480
Jacob Stella <jacobs5118@gmail.com>
2024-03-05 17:46:32 -0500

    imported register page to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (

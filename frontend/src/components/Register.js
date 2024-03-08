f644f1e7904c387deab3e6fd75d71013a6e1e544
Jack Rauch <jwrauch02@gmail.com>
2024-03-08 12:21:35 -0500

    PLEASE LET ME REGISTER

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -18,7 +18,7 @@ function Register() {
         }
 
         try {
-            const response = await fetch('https://largeprojectgroup3-efcc1eed906f.herokuapp.com/', {
+            const response = await fetch('https://largeprojectgroup3-efcc1eed906f.herokuapp.com/api/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',

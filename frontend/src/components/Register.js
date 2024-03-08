e95f01701a364777cdf56eaf3595a2f32bcff826
Jack Rauch <jwrauch02@gmail.com>
2024-03-08 11:43:56 -0500

    fixed register link from local host to heroku link

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -18,7 +18,7 @@ function Register() {
         }
 
         try {
-            const response = await fetch('http://localhost:5000/api/register', {
+            const response = await fetch('https://largeprojectgroup3-efcc1eed906f.herokuapp.com/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',

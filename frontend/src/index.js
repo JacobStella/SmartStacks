34f71f7395ab25ab7a53606b309c7bbb4e683316
AaronRE <102276426+AaronRE@users.noreply.github.com>
2024-03-13 19:50:01 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import * as b from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';

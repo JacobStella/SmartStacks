40a044a26bff4ea9aaf3e7af3c1c79b91f21638f
AaronRE <102276426+AaronRE@users.noreply.github.com>
2024-03-14 00:00:44 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,8 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
 
 
 
@@ -12,7 +13,7 @@ function Layout() {
   return (
     <div className = "container">
       <div className = "row">
-        <div className = "col"></div>
+        <div className = "col-sm-1"></div>
         <div className="col"></div>
         <div className="col"> 
           <Button variant = "primary">Plus</Button>

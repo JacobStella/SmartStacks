f3098c5cd150881bcce5b2f8fb8fb446047e1cf7
Knight-mitchell88 <knight.mitchell88@gmail.com>
2024-03-15 15:32:29 -0400

    Co-authored-by: JacobStella <JacobStella@users.noreply.github.com>

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,84 +4,85 @@ function toRegister(){
     window.location.href = '/register';
 }
 
-function Login() {
+function Login() 
+{
     var loginName;
     var loginPassword;
 
     const [message,setMessage] = useState('');
     const app_name = 'largeprojectgroup3-efcc1eed906f'
+    
     function buildPath(route)
     {
-    if (process.env.NODE_ENV === 'production')
-    {   
-    return 'https://' + app_name + '.herokuapp.com/' + route;
-    }   
-    else
-    {
-    return 'http://localhost:5000/' + route;
-    }
-}
-
-
-const doLogin = async event => 
-{
-    event.preventDefault(); // Prevent the default form submit behavior
-
-    var obj = {login:loginName.value,password:loginPassword.value};
-    var js = JSON.stringify(obj);
-
-    try
-    {    
-        const response = await fetch(buildPath('api/login'), {
-            method:'POST',
-            body:js,
-            headers:{'Content-Type': 'application/json'}
-        });
-
-        // Check if the login was successful based on the response status
-        if(response.status === 200) // Successful login
+        if (process.env.NODE_ENV === 'production')
+        {   
+            return 'https://' + app_name + '.herokuapp.com/' + route;
+        }   
+        else
         {
-            var res = await response.json(); // Parse JSON response
-            var user = {firstName:res.firstName, lastName:res.lastName, id:res.id};
-            localStorage.setItem('user_data', JSON.stringify(user));
-            setMessage('');
-            window.location.href = '/cards'; // Redirect to another route/page
-        }
-        else if(response.status === 400) // Incorrect credentials
-        {
-            setMessage('User/Password combination incorrect');
-        }
-        else // Other kinds of errors
-        {
-            setMessage('An error occurred. Please try again.');
+            return 'http://localhost:5000/' + route;
         }
     }
-    catch(e)
+
+
+    const doLogin = async event => 
     {
-        alert(e.toString());
-        return;
-    }    
-};
+        event.preventDefault(); // Prevent the default form submit behavior
+
+        var obj = {login:loginName.value,password:loginPassword.value};
+        var js = JSON.stringify(obj);
+
+        try
+        {    
+            const response = await fetch(buildPath('api/login'), 
+            {
+                method:'POST',
+                body:js,
+                headers:{'Content-Type': 'application/json'}
+            });
+
+            // Check if the login was successful based on the response status
+            if(response.status === 200) // Successful login
+            {
+                var res = await response.json(); // Parse JSON response
+                var user = {firstName:res.firstName, lastName:res.lastName, id:res.id};
+                localStorage.setItem('user_data', JSON.stringify(user)); //stores users info for later use
+                setMessage(''); //wtf is this for 
+                window.location.href = '/cards'; // this will have to be changed is this is a popup, OR if this is a seperate page. We want to return to the page login called from
+            }
+            else if(response.status === 400) // Incorrect credentials
+            {
+                setMessage('Incorrect Login info');
+            }
+            else // Other kinds of errors
+            {
+                setMessage('An error occurred. Please try again.');
+            }
+        }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }    
+    };
 
 
 
 
-return (
-    <div id="loginDiv">
-        <form onSubmit={doLogin}> {/* Note the onSubmit handler here */}
-            <span id="inner-title">PLEASE LOG IN</span><br />
-            <input type="text" id="loginName" placeholder="Username" 
-ref={(c) => loginName = c} /><br />
-<input type="password" id="loginPassword" placeholder="Password" 
-ref={(c) => loginPassword = c} /><br />
-
-            <input type="submit" id="loginButton" className="buttons" value="Do It" />
-            {/* Changed to type="button" and corrected class to className */}
-            <input type="button" id="registerButton" className="buttons" value="Register"
-                onClick={toRegister} />
-        </form>
-        <span id="loginResult">{message}</span>
-    </div>
-);
+    return 
+    (
+        <div id="loginDiv">
+            <form onSubmit={doLogin}> {/* Note the onSubmit handler here */}
+                <span id="inner-title">PLEASE LOG IN</span><br />
+                <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} /><br />
+                <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} /><br />
+                <input type="submit" id="loginButton" className="buttons" value="Login" />
+                {/* Changed to type="button" and corrected class to className */}
+                <span id="inner-title">Don't have an account?</span> <a href="https://largeprojectgroup3-efcc1eed906f.herokuapp.com/register">Register</a>
+                <input type="button" id="registerButton" className="buttons" value="Register" onClick={toRegister} />
+            </form>
+            <span id="loginResult">{message}</span>
+        </div>
+    );
 }
 export default Login;

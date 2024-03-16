c5c13f7ea91498a4889096cfc0d5e408b872d7e2
JacobStella <jacobs5118@gmail.com>
2024-03-16 14:44:33 -0400

    balls

---
+++ frontend/src/pages/LandingPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+import LoggedInName from '../components/LoggedInName';
+//import CreateLink from '../components/CreateLink'; 
+//import LibraryLink from '../components/LibraryLink'; 
+//import BrowseLink from '../components/BrowseLink'; 
+//import PlayLink from '../components/PlayLink'; 
+//import GetStarted from '../components/GetStarted'; 
+import '../Web.css';
+
+const LandingPage = () => {
+    return (
+        <div className="page-container">
+            <PageTitle />
+            <LoggedInName />
+            {/*<div className="main-content">
+                <CreateLink />
+                <LibraryLink />
+                <BrowseLink />
+                <PlayLink />
+            </div>
+    <GetStarted />*/}
+        </div>
+    );
+}
+
+export default LandingPage;

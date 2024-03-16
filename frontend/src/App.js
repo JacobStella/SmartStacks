e182592519b83fd250d10dde49e2c3608887c9d4
JacobStella <jacobs5118@gmail.com>
2024-03-16 17:20:33 -0400

    Co-authored-by: Knight-mitchell88 <Knight-mitchell88@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage';
+
 import CardPage from './pages/CardPage';
-import RegisterPage from './pages/RegisterPage';
+import CreateStackPage from './pages/CreateStackPage';
+import ForgorPage from './pages/ForgorPage';
+import GamePage from './pages/GamePage';
 import LandingPage from './pages/LandingPage';
+import LibraryPage from './pages/LibraryPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import StudyPage from './pages/StudyPage';
+import UserSettingsPage from './pages/UserSettingsPage';
+import ViewStackPage from './pages/ViewStackPage';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
         <Route path="/cards" element={<CardPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/create" element={<CreateStackPage />} />
+        <Route path="/forgor" element={<ForgorPage />} />
+        <Route path="/game" element={<GamePage />} />
         <Route path="/landing" element={<LandingPage />} />
+        <Route path="/library" element={<LibraryPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/study" element={<StudyPage />} />
+        <Route path="/settings" element={<UserSettingsPage />} />
+        <Route path="/view" element={<ViewStackPage />} />
       </Routes>
     </BrowserRouter>
   );

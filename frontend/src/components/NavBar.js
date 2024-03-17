c1a3a05d1d4cb8621e86c791a930a5fb9cf00ef0
JacobStella <jacobs5118@gmail.com>
2024-03-17 12:59:49 -0400

    updated web.css for vert and nav bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './web.css'; // Make sure this import points to where your web.css file is located
+import '../Web.css'; 
 
 const NavBar = ({ isLoggedIn, userProfilePic }) => {
   return (

1091ffcf6f55b46e9b73167db674a9af0fd551ad
jrauch18 <118935935+jrauch18@users.noreply.github.com>
2024-03-18 18:28:07 -0400

    im so close

--- frontend/src/components/CardUI.js
+++ frontend/src/components/CardUI.js
@@ -6,7 +6,7 @@ function CardUI() {
     const [message, setMessage] = useState('');
     const [searchResults, setResults] = useState('');
     const [cardList, setCardList] = useState('');
-    const [setName, setSetName] = useState('');
+    const [SetName, setSetName] = useState('');
     const [isPublic, setIsPublic] = useState(false);
     const [className, setClassName] = useState('');
     const [setIds, setSetIds] = useState([]);
@@ -83,7 +83,7 @@ function CardUI() {
         event.preventDefault();
         // Assuming `cards` is an array of objects, each representing a card with its own properties
         // For example, cards might look like: [{ term: "Term 1", definition: "Definition 1" }, { term: "Term 2", definition: "Definition 2" }]
-        let obj = { userId: userId, setName: setName, public: isPublic, cards: cards };
+        let obj = { UserId: userId, SetName: setName, public: isPublic, cards: cards };
         let js = JSON.stringify(obj);
     
         try {

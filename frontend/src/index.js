2045169aa249ae5ed1e5f89010948664fba96a54
MarvinOro <0marvino02@gmail.com>
2024-03-18 21:28:44 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -73,7 +73,7 @@ rootFooter.render(
     <LayoutFooter />
   </React.StrictMode>
 );
-
+// hello
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

ec980c9704270f03740b5180b1e26b523732670e
Wess A <114402795+DaimondSailer@users.noreply.github.com>
2024-03-20 18:18:25 -0400

    Update server.js
    a

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.post('/api/deletecard', async (req, res, next) => {
 });
 
 // Update Card
-app.post('/api/updatecard', async (req, res, next) => {
+app.put('/api/updatecard', async (req, res, next) => {
 	const { cardId, newTerm } = req.body; // cardId of card to be updated
 
 	// Running command

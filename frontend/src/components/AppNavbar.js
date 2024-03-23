fb8c5f94813d88aa0a3817254899566253d6fead
JacobStella <jacobs5118@gmail.com>
2024-03-23 13:02:09 -0400

    all my homies hate bootstrap

--- frontend/src/components/AppNavbar.js
+++ frontend/src/components/AppNavbar.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import { Container, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom'; // Import Link
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'; // Make sure this is installed too
 
 const AppNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand as={Link} to="/landing">YourApp</Navbar.Brand> {/* Updated */}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/landing">Home</Nav.Link> {/* Updated */}
-            <Nav.Link as={Link} to="/library">Library</Nav.Link> {/* Updated */}
-            <Nav.Link as={Link} to="/create">Create</Nav.Link> {/* Updated */}
-          </Nav>
-          <Form className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
+      <Navbar.Brand><LinkContainer to="/landing"><Nav.Link>Home</Nav.Link></LinkContainer></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <LinkContainer to="/landing">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/library">
+            <Nav.Link>Library</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/create">
+            <Nav.Link>Create</Nav.Link>
+          </LinkContainer>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
     </Navbar>
   );
 };

85a771b5775ab6c0324defc4f29e82790c0d9525
JacobStella <jacobs5118@gmail.com>
2024-03-23 13:42:16 -0400

    fml

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -10,7 +10,50 @@ import '../Web.css';
 const LandingPage = () => {
     
     return (
-        <div className="page-container">
+        
+
+<div className="content-container"> 
+<div className="links-and-started">
+    <div className="link-grid">
+        <div className="link-row">
+            <div className="link-column">
+                <CreateLink />
+            </div>
+            <div className="link-column">
+                <LibraryLink />
+            </div>
+        </div>
+        <div className="link-row">
+            <div className="link-column">
+                <BrowseLink />
+            </div>
+            <div className="link-column">
+                <PlayLink />
+            </div>
+        </div>
+    </div>
+    <div className="vertical-line"></div>
+    <GetStarted />
+</div>
+</div>
+        
+    );
+}
+
+export default LandingPage;
+
+{/*
+             code relating to the navbar clickable profile pic
+            <NavBar isLoggedIn={isLoggedIn} userProfilePic={userProfilePic} />
+            */}
+{/*
+    code relating to the navbar clickable profile pic
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // Default to not logged in
+    const [userProfilePic, setUserProfilePic] = useState('path_to_default_profile_pic.png');
+    */}
+{/*
+
+<div className="page-container">
             <NavBar2 />
             <div className="container">
             <div className="row">
@@ -31,46 +74,5 @@ const LandingPage = () => {
             </div>
             </div>
         </div>
-
-        
-        
-    );
-}
-
-export default LandingPage;
-
-{/*
-             code relating to the navbar clickable profile pic
-            <NavBar isLoggedIn={isLoggedIn} userProfilePic={userProfilePic} />
-            */}
-{/*
-    code relating to the navbar clickable profile pic
-    const [isLoggedIn, setIsLoggedIn] = useState(false); // Default to not logged in
-    const [userProfilePic, setUserProfilePic] = useState('path_to_default_profile_pic.png');
-    */}
-{/*
-            <div className="content-container"> 
-                <div className="links-and-started">
-                    <div className="link-grid">
-                        <div className="link-row">
-                            <div className="link-column">
-                                <CreateLink />
-                            </div>
-                            <div className="link-column">
-                                <LibraryLink />
-                            </div>
-                        </div>
-                        <div className="link-row">
-                            <div className="link-column">
-                                <BrowseLink />
-                            </div>
-                            <div className="link-column">
-                                <PlayLink />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="vertical-line"></div>
-                    <GetStarted />
-                </div>
-            </div>
+            
             */}

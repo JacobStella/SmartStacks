c2df7a538731e39210a708b31b818aab2d5a1784
Knight-mitchell88 <knight.mitchell88@gmail.com>
2024-03-23 21:46:23 -0400

    Updated Navbar for logo.

--- frontend/src/components/NavBar2.js
+++ frontend/src/components/NavBar2.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Web.css'; 
+// Import your logo image if it's stored locally
+import logo from '../images/browse.png'; // Update this path to where your actual logo is stored
 
 const NavBar2 = () => {
     return (
         <nav className="navbar">
+            {/* Insert the img tag for the logo with the className "navbar-logo" */}
+            <img src={logo} alt="Company Logo" className="navbar-logo" />
+            
             <Link to="/landing" className="nav-button">Home</Link>
             <Link to="/library" className="nav-button">Library</Link>
             <Link to="/create" className="nav-button">Create</Link>

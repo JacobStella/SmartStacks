a82fef5896235d2fab931b32cf6cf8ff5df6496d
MarvinOro <0marvino02@gmail.com>
2024-03-24 04:31:30 -0400

    What the Library.js skeleton should've been
    made the skeleton the way I actually wanted to. I started making the navBar but I don't know how installing 3rd party stuff from the console will work when you test it

--- frontend/mobile/mobile/src/components/Library.js
+++ frontend/mobile/mobile/src/components/Library.js
@@ -3,52 +3,98 @@ import {Button, StyleSheet, TextInput, SafeAreaView, View, Text} from "react-nat
 
 const Library = () => {
     return (
+        
         <View style={styles.container}>
             <Text style={styles.title}>Library</Text>
             <View style={styles.subheader}>
-                <Text style={styles.subheaderTitleLeft}>Classes</Text>
+                <Text style={styles.subheaderTitle}>Classes</Text>
+            </View>
+            <View style={styles.iconContainer}>
+                <View style={{flexDirection: 'column'}}>
+                    <View style={styles.iconOutline}>
+                        <Text>This is where the images/previews will go</Text>
+                    </View>
+                    <Text style={styles.iconTitle}>Title</Text>
+                </View>
+                <View style={{flexDirection: 'column'}}>
+                    <View style={styles.iconOutline}>
+                        <Text>Will put the respective URL for boxes as well</Text>
+                    </View>
+                    <Text style={styles.iconTitle}>Title</Text>
+                </View>
             </View>
             <View style={styles.line} />
             <View style={styles.subheader}>
                 <Text style={styles.subheaderTitle}>Stacks</Text>
             </View>
+            <View style={styles.iconContainer}>
+                <View style={{flexDirection: 'column'}}>
+                    <View style={styles.iconOutline}>
+                        <Text>Might consider not using iconOutline</Text>
+                    </View>
+                    <Text style={styles.iconTitle}>Title</Text>
+                </View>
+            </View>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        alignItems: 'flex-start',
         paddingHorizontal: 20,
         paddingTop: 50,
     },
     title: {
-        fontSize: 24,
-        fontWeight: "bold",
-        textAlign: "center",
-        marginBotton: 20,
+        fontSize: 35,
+        fontWeight: 'bold',
+        color: 'purple',
+        textAlignVertical: 'top',
+        marginBottom: 50,
     },
     subheader: {
-        flexDirection: "row",
-        alignItems: "center",
-        marginBottom: 10,
+        flexDirection: 'row',
     },
     subheaderTitle: {
-        fontSize: 20,
-        fontWeight: "bold",
+        flex:1,
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: 'red',
         marginLeft: 10,
+        marginBottom: 10
     },
-    subheaderTitleLeft: {
-        fontSize: 20,
-        fontWeight: "bold",
+    iconContainer: {
+        flexDirection: 'row',
+        paddingLeft: 10,
+    },
+    iconOutline: {
+        width: 100,
+        height: 70,
+        backgroundColor: 'yellow',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingLeft: 5,
+        paddingRight: 5,
+        marginBottom: 5,
+        marginRight: 20,
+    },
+    iconTitle: {
+        fontSize: 16,
+        textDecorationLine: 'underline',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        justifyContent: 'center',
+        paddingRight: 30,
+        marginBottom: 20,
+
     },
     line: {
-        borderBottomWidth: 1,
-        borderColor: "black",
-        width: "100%",
-        marginBottom: 10,
+        borderBottomColor: 'black',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        alignSelf: 'stretch',
+        marginBottom: 20,
     },
 });
 

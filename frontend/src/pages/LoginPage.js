4524886c9366e699518ddc44efd98373f155e7c1
Knight-mitchell88 <knight.mitchell88@gmail.com>
2024-03-26 17:40:11 -0400

    Login Page fix (hopefully)

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,23 +1,21 @@
-
-//comment
 import React from 'react';
 import PageTitle from '../components/PageTitle';
 import Login from '../components/Login';
-import '../LoginRegister.css';
 import NavBar2 from '../components/NavBar2';
 import LandingFooter from '../components/LandingFooter';
+import '../LoginRegister.css';
 
 const LoginPage = () => {
     return (
-        <div className="login-container">
+        <>
             <NavBar2 />
-            <div>
+            <div className="login-container">
+                <PageTitle />
                 <Login />
             </div>
             <LandingFooter />
-        </div>
+        </>
     );
 };
-
 
 export default LoginPage;

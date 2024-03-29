122dccb7ddc843f205a495b0465f2a02bcbaa7e4
Jacob Stella <jacobs5118@gmail.com>
2024-03-29 18:41:29 -0400

    now this better work

--- frontend/src/components/FolderStacksDisplay.js
+++ frontend/src/components/FolderStacksDisplay.js
@@ -1,6 +1,8 @@
 // FolderStacksDisplay.js
 
 import React from 'react';
+import EditIcon from '../images/EditIcon';
+import FolderIcon from '../images/FolderIcon';
 import '../Library.css';
 
 const FolderTemplate = ({ name }) => {
@@ -8,17 +10,16 @@ const FolderTemplate = ({ name }) => {
         <div className="folder-template">
             <div className="folder-image">
                 {/* Placeholder for folder icon */}
-                <img src="./images/FolderIcon.png" alt="Folder" />
+                <img src={FolderIcon} alt="Folder" />
             </div>
             <span className="folder-name">{name}</span>
             <button className="folder-action-button">
                 {/* Placeholder for button icon */}
-                <img src="./images/EditIcon.png" alt="Action" />
+                <img src={EditIcon} alt="Action" />
             </button>
         </div>
     );
 };
-
 const FolderStacksDisplay = () => {
     // Sample data - eventually this would be dynamic
     const folders = [

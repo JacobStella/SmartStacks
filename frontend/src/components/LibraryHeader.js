1c688a10dfbb6cb0609c5301f69ead6e163cef1e
Jacob Stella <jacobs5118@gmail.com>
2024-03-29 14:41:34 -0400

    basic library css file

--- frontend/src/components/LibraryHeader.js
+++ frontend/src/components/LibraryHeader.js
@@ -5,25 +5,28 @@ const LibraryHeader = () => {
     return (
         <header className="library-header">
             <h1>Your Library</h1>
-            {/* Search bar */}
-            <div className="search-container">
-                <input type="text" placeholder="Search your library..." className="search-input"/>
-            </div>
-            {/* Button for creating a new folder */}
-            <button className="new-folder-btn">Create New Folder</button>
-            {/* Dropdown menu for filters */}
-            <div className="filter-dropdown">
-                <select className="filter-select">
-                    <option value="">Select Filter</option>
-                    <option value="date-asc">Date Ascending</option>
-                    <option value="date-desc">Date Descending</option>
-                    <option value="name-asc">Name Ascending</option>
-                    <option value="name-desc">Name Descending</option>
-                    <option value="type">Type</option>
-                </select>
+            <div className="header-controls"> {/* Wrapper div for horizontal layout */}
+                {/* Search bar */}
+                <div className="search-container">
+                    <input type="text" placeholder="Search your library..." className="search-input"/>
+                </div>
+                {/* Button for creating a new folder */}
+                <button className="new-folder-btn">Create New Folder</button>
+                {/* Dropdown menu for filters */}
+                <div className="filter-dropdown">
+                    <select className="filter-select">
+                        <option value="">Select Filter</option>
+                        <option value="date-asc">Date Ascending</option>
+                        <option value="date-desc">Date Descending</option>
+                        <option value="name-asc">Name Ascending</option>
+                        <option value="name-desc">Name Descending</option>
+                        <option value="type">Type</option>
+                    </select>
+                </div>
             </div>
         </header>
     );
 };
+
 
 export default LibraryHeader;

e888a418405aaa1cf9347c717db381c348d2a7e7
Knight-mitchell88 <knight.mitchell88@gmail.com>
2024-04-02 18:33:36 -0400

    add forgor

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -83,6 +83,7 @@ function Login()
                     <span id="inner-title">PLEASE LOG IN</span><br />
                     <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} /><br />
                     <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} /><br />
+                    <a href="https://largeprojectgroup3-efcc1eed906f.herokuapp.com/forgor">Forgot Password</a>
                     <input type="submit" id="loginButton" className="buttons" value="Login" /><br />
                     <span id="inner-title">Don't have an account?</span> <a href="https://largeprojectgroup3-efcc1eed906f.herokuapp.com/register">Register</a>
                 </form>

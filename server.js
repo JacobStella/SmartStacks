c202eb9145028c935d0111e38a3378a1de30b08a
jrauch18 <118935935+jrauch18@users.noreply.github.com>
2024-04-02 19:45:29 -0400

    this also might fuck some shit up

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers',
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
         'GET, POST, PATCH, DELETE, OPTIONS'
     );
     next();
-});
+});>*/
 
 
 

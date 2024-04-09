180505f10516a9a09308241d77d782a43f5c56ec
Knight-mitchell88 <knight.mitchell88@gmail.com>
2024-04-09 18:37:44 -0400

    oops

--- frontend/src/components/FolderStacksDisplay.js
+++ frontend/src/components/FolderStacksDisplay.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import FolderIcon from '../images/FolderIcon.png';
 import EditIcon from '../images/EditIcon.png';

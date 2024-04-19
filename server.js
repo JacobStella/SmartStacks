cfaac4da133570adf38eac0345b3d837777807a9
Wess A <114402795+DaimondSailer@users.noreply.github.com>
2024-04-19 14:14:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -97,6 +97,7 @@ app.post('/api/send-verif', async (req, res) => {
 	const token = crypto.randomBytes(32).toString('hex');
 
 	// Save token to userId
+	 const db = client.db("Group3LargeProject");
 	const result = await db.collection('Users').updateOne({ "_id": new ObjectId(userId) }, {$set: {Token:token}});
 	// make sure above line succeeded
 	if (!result){

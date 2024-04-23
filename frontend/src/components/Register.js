7407a7deb385fa1319edbe906de2ba668084a6d1
Knight-mitchell88 <knight.mitchell88@gmail.com>
2024-04-23 13:40:09 -0400

    add pop up

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -9,6 +9,11 @@ function Register() {
     var email; // Added for email input
     var userId;
     const [message, setMessage] = useState('');
+    const [showPopup, setShowPopup] = useState(false); // State to control the visibility of the popup
+
+    const renderPopup = () => {
+        setShowPopup(true);
+    };
 
     const doRegister = async event => {
         event.preventDefault();
@@ -75,97 +80,19 @@ function Register() {
                 <input type="email" id="email" placeholder="Email" ref={(c) => email = c} />
                 <input type="password" id="registerPassword" placeholder="Password" ref={(c) => registerPassword = c} />
                 <input type="password" id="confirmPassword" placeholder="Confirm Password" ref={(c) => confirmPassword = c} />
-                <input type="submit" id="registerButton" className="buttons" value="Register" />
+                {/* Add onClick event handler to the Register button */}
+                <input type="button" id="registerButton" className="buttons" value="Register" onClick={renderPopup} />
                 <a href="/login" className="back-to-login">&lt; Back to Login</a>
             </form>
             <span id="registerResult">{message}</span>
+            {/* Render the pop-up textbox based on the state */}
+            {showPopup && (
+                <div className="popup">
+                    <span className="popuptext" id="myPopup">Popup Textbox</span>
+                </div>
+            )}
         </div>
     );
 };
 
 export default Register;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*ONE ONE
-import React, { useState } from 'react';
-
-function Register() {
-    var registerName;
-    var registerPassword;
-    var confirmPassword;
-    var firstName;
-    var lastName;
-    var email; // Added for email input
-    const [message, setMessage] = useState('');
-
-    const doRegister = async event => {
-        event.preventDefault();
-
-        if(registerPassword.value !== confirmPassword.value) {
-            setMessage('Passwords do not match!');
-            return;
-        }
-
-        try {
-            const response = await fetch('https://largeprojectgroup3-efcc1eed906f.herokuapp.com/api/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    email: email.value, // Include email in the request
-                    firstName: firstName.value,
-                    lastName: lastName.value,
-                    username: registerName.value,
-                    password: registerPassword.value,
-                }),
-            });
-
-            if(response.ok) {
-                setMessage('User registered successfully');
-                // Consider clearing the form or redirecting the user after successful registration
-                window.location.href = "https://largeprojectgroup3-efcc1eed906f.herokuapp.com/login";
-            } else {
-                setMessage('Failed to register user');
-            }
-        } catch (error) {
-            setMessage('Failed to register user');
-        }
-    };
-
-    return (
-        <div id="registerDiv">
-            <h1 id="title">Welcome to Smart Stacks!</h1>
-            <form onSubmit={doRegister}>
-                <span id="inner-title">PLEASE REGISTER</span><br />
-                <input type="text" id="firstName" placeholder="First Name" ref={(c) => firstName = c} />
-                <input type="text" id="lastName" placeholder="Last Name" ref={(c) => lastName = c} />
-                <input type="text" id="registerName" placeholder="Username" ref={(c) => registerName = c} />
-                <input type="email" id="email" placeholder="Email" ref={(c) => email = c} />
-                <input type="password" id="registerPassword" placeholder="Password" ref={(c) => registerPassword = c} />
-                <input type="password" id="confirmPassword" placeholder="Confirm Password" ref={(c) => confirmPassword = c} />
-                <input type="submit" id="registerButton" className="buttons" value="Register" />
-                <a href="/login" className="back-to-login">&lt; Back to Login</a>
-            </form>
-            <span id="registerResult">{message}</span>
-        </div>
-    );
-};
-
-export default Register;
-*/

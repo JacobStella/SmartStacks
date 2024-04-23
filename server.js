777bd8d696d6bff6f940dc34acde294855aa1f43
Wess A <114402795+DaimondSailer@users.noreply.github.com>
2024-04-23 15:38:49 -0400

    Update server.js
    removed forgot hashing

--- server.js
+++ server.js
@@ -209,9 +209,6 @@ app.post('/api/forgot', async (req, res) => {
 	
 	try{
 		const db = client.db("Group3LargeProject");
-		// Hash password
-      		const hashedPassword = crypto.pbkdf2Sync(newPass, salt, 10, 64, 'sha512').toString('hex');
-		const result = await db.collection('Users').updateOne({"_id": new ObjectId(userId)}, {$set: {Password:hashedPassword}});
 		
 		if(!result){
 			res.status(500).json({message: "Could not change pass"});
